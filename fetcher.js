24d7a602bd87243c36efe7936d239b3209fe4117
Situ2000 <yuanyuansitu@gmail.com>
2022-11-30 18:52:01 +0000

    modified

--- fetcher.js
+++ fetcher.js
@@ -4,14 +4,14 @@ const fs = require('fs');
 const urlRequest = process.argv[2];
 const filePath = process.argv[3];
 
-function example() {
+function fetcher() {
   request(urlRequest, (error, response, body) => { 
-
     fs.writeFile(filePath, body, err => {
       if (err) {
-        console.error(err);
+        return console.error(err);
       }
+      console.log(`Downloaded and saved ${body.length} bytes to ${filePath}`);
     })
   })
 }
-example();
+fetcher();
